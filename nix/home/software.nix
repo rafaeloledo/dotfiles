250c41205b2451907c6998a2773d2a2fba68efa8
rafaeloledo <rafaeloliveiraledo@gmail.com>
2025-01-14 02:01:23 -0300

    chore: update config

--- nix/home/software.nix
+++ nix/home/software.nix
@@ -102,7 +102,6 @@ in
 
 {
   imports = [
-    ./firefox.nix
     ./git.nix
     ./wayland.nix
     ./terminal.nix
@@ -121,5 +120,34 @@ in
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-gtk;
+  };
+
+  programs.firefox = {
+    enable = true;
+    profiles.default.extraConfig = ''
+      user_pref("ui.key.menuAccessKeyFocuses", false);
+      user_pref("extensions.pocket.enabled", false);
+      user_pref("full-screen-api.transition-duration.enter", "0 0");
+      user_pref("full-screen-api.transition-duration.leave", "0 0");
+      user_pref("full-screen-api.transition.timeout", 0);
+      user_pref("full-screen-api.warning.timeout", 0);
+      user_pref("full-screen-api.warning.delay", -1);
+      user_pref("browser.tabs.firefox-view", false);
+      user_pref("browser.uitour.enabled", false);
+
+      //user_pref("browser.cache.disk.enable", false);
+      // Graphics
+      //user_pref("gfx.webrender.all", true);
+      //user_pref("gfx.webrender.precache-shaders", true);
+      //user_pref("gfx.webrender.compositor", true);
+      //user_pref("gfx.canvas.accelerated", true);
+      //user_pref("layers.gpu-process.enabled", true);
+      //user_pref("media.hardware-video-decoding.enabled", true);
+
+      user_pref("browser.newtabpage.activity-stream.default.sites", "");
+      user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+      user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+      user_pref("signon.rememberSignons", false);
+    '';
   };
 }
